23a24265ffc2dd97b4c09f9484db6026193843b8
Punkoleo <punkoleo@Doliprane>
2016-03-30 20:25:33 +0200

    DHCPD Config | Mise en place automatique du fichier de conf

--- MagicPi/models/DhcpConfig.py
+++ MagicPi/models/DhcpConfig.py
@@ -4,7 +4,32 @@ class DhcpConfigModel:
 
     def __init__(self, Environement):
         self.env = Environement
+        self.BDDmodel = self.env.getModel('BDD')
+        self.HostInformationModel = self.env.getStatic('HostInformation')
 
 
-    def createFileConfig(self,HostInformationModel):
-        return "Hello World";
+    def getFileConfig(self):
+
+        iface = self.BDDmodel.activeInterface()
+        infoHost = self.BDDmodel.selectFromBDD('HostInfo');
+
+        subnet = infoHost[iface]['SUBNET']
+        mask = infoHost[iface]['MASK'];
+        broadcast = infoHost[iface]['BROADCAST']
+        rangeAdress = self.getRange(self.HostInformationModel.getInfoForInterface(iface)[0]['addr']);
+
+        fileDhcp = 'default-lease-time 600;\n' \
+                   'max-lease-time 7200;\n' \
+                   'option subnet-mask '+mask+';\n' \
+                   'option broadcast-address '+broadcast+';\n' \
+                   'option domain-name-servers 8.8.8.8, 8.8.4.4;\n\n' \
+                   'subnet '+subnet+' netmask '+mask+' {\n' \
+                   '    range '+rangeAdress+';\n' \
+                   '}\n' \
+                   ''
+        return fileDhcp;
+
+    def getRange(self,addr):
+        listNum = str(addr).split('.');
+        base = listNum[0]+'.'+listNum[1]+'.'+listNum[2]
+        return base+'.2 '+base +'.253'
