1c21557c86976ed19538067edf7b276a22c15f9f
Picot Léonard <picot.leonard@gmail.com>
2016-04-23 15:03:26 +0200

    Update WatcherAction.py
    Modification des stdout

--- MagicPi/actions/WatcherAction.py
+++ MagicPi/actions/WatcherAction.py
@@ -12,7 +12,7 @@ class WatcherAction:
     def run(self):
         pool = mp.ProcessingPool(nodes=1)
         pool.amap(WatcherAction.asynchWatcher,[self])
-        print "Watcher en cours d'execution"
+        print "[*] Watcher is running..."
 
     def asynchWatcher(self):
         # On recupere une premiere fois les infos :
@@ -23,7 +23,7 @@ class WatcherAction:
         while (modelHostInfo.interfaces == networkRef) :
             time.sleep(20);
             modelHostInfo.loadInformation();
-            print 'Watcher : verification en cours...'
+            print "Watcher is watching..."
 
         # Vu que ca a ete modifie, on relance l'appli
         print "\n\n\t<==================>\n\n Watcher : Les infos ont change, on relance l'application"
